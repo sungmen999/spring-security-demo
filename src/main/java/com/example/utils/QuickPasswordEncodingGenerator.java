b701c808c9bf5e482b07e704f526a2a87259ee1f
Watcharapong Nanthasan <wacharapong@exzyteam.com>
2016-10-08 13:02:31 +0700

    comment QuickPasswordEncodingGenerator class and add minifest.yml

--- src/main/java/com/example/utils/QuickPasswordEncodingGenerator.java
+++ src/main/java/com/example/utils/QuickPasswordEncodingGenerator.java
@@ -11,11 +11,11 @@ public class QuickPasswordEncodingGenerator {
     /**
      * @param args
      */
-    public static void main(String[] args) {
-        String password = "password";
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        System.out.println(passwordEncoder.encode(password));
-    }
+//    public static void main(String[] args) {
+//        String password = "password";
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        System.out.println(passwordEncoder.encode(password));
+//    }
 
 }
 
